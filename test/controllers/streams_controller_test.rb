3a18022265167aaa644a7ee567aba643f20d5250
Mitchell Gould <mitchellgould7@gmail.com>
2022-11-09 14:17:41 -0500

    return the tests

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -42,7 +42,13 @@ class TurboClone::StreamsControllerTest < ActionDispatch::IntegrationTest
     article = Article.create! content: "something"
     get article_path(article), as: :turbo_stream
 
-    assert_dom_equal %(<turbo-stream target="article_1" action="remove"></turbo-stream>), @response.body
+    assert_dom_equal <<~HTML, @response.body
+      <turbo-stream action="remove" target="#{dom_id(article)}"></turbo-stream>
+      <turbo-stream action="update" target="#{dom_id(article)}"><template>#{render(article)}</template></turbo-stream>
+      <turbo-stream action="replace" target="#{dom_id(article)}"><template>Something else</template></turbo-stream>
+      <turbo-stream action="prepend" target="articles"><template>#{render(article)}</template></turbo-stream>
+      <turbo-stream action="prepend" target="articles"><template>#{render(article)}</template></turbo-stream>
+    HTML
   end
 
 
